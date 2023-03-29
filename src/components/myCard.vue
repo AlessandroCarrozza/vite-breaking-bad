d111674bd5f3be80ad4a5ce2673cc3dbe57cd322
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-03-29 18:27:29 +0200

    Progetto completato

--- src/components/myCard.vue
+++ src/components/myCard.vue
@@ -14,16 +14,27 @@ export default {
 <template>
     <div class="card">
         <img :src="image" :alt="name">
+        <h3>{{ name }}</h3>
+        <h4>{{ archetype }}</h4>
     </div>
 </template>
 
 
 <style scoped lang="scss">
 .card {
-    max-width: calc(100% / 5);
+    max-width: calc(100% / 5 - 1rem);
+    text-align: center;
+    background-color: orange;
+    margin: 0.5rem;
+    padding-bottom: 1rem;
 
     img {
         width: 100%;
+    }
+
+    h3 {
+        padding: 1rem;
+        color: white;
     }
 }
 </style>
