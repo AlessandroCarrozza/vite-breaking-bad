a553424d7c08ec28219baa6b2321df00c478029d
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-03-30 18:09:16 +0200

    Risoluzione errore select

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { reactive } from "vue"
 
 export const store = reactive({
     cardsList: [],
-    userChoice: "",
+    userChoice: "All Cards",
     archetypeList: ["Alien", "Noble Knight", "Melodious", "Archfiend", "Elemental HERO", "Umi",
         "ABC", "@Ignister", "Unchained", "Gem-", "Rokket", "Abyss Actor"]
 })
